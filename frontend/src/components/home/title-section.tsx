d93f4ce266cde2f3e5bccbee920ba9aa3da03b07
DaryxXx <15078855+DaryxXx@users.noreply.github.com>
2026-05-31 02:31:18 +0200

    Update home title to Automata Operator

--- frontend/src/components/home/title-section.tsx
+++ frontend/src/components/home/title-section.tsx
@@ -2,9 +2,9 @@ export default function TitleSection() {
   return (
     <div className="animate-slide-up">
       <h1 className="w-full text-center mb-3 text-3xl md:text-5xl leading-tight font-bold text-gray-800 dark:text-white tracking-tight text-balance">
-        Fully Permissionless and Incentivized{" "}
+        Automata{" "}
         <span className="font-extrabold bg-gradient-primary bg-clip-text text-transparent">
-          Web&nbsp;Operator
+          Operator
         </span>
       </h1>
       <h2 className="w-full text-center mb-3 text-xl md:text-2xl font-medium text-gray-600 dark:text-gray-300 tracking-tight">
